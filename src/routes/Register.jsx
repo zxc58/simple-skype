4afd488ee7247af3000dc6b39154d6c0020a8bab
zxc58 <abc01234567889@gmail.com>
2022-12-07 20:03:58 +0800

    modify register page

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { InputGroup, Form, Button, Container } from 'react-bootstrap'
@@ -14,22 +15,29 @@ export default function Register (props) {
     navigate('/')
   }
   return (
-    <Container>
-      <h1 className='text-center'>Register</h1>
-      <form className='mt-3 text-center' id='register-form' onSubmit={registerUser}>
-        <InputGroup className="mb-3">
+    <Container style={{ maxWidth: '600px' }}>
+      <h1 className='text-center py-6'>Register</h1>
+      {/* <form className='mt-3 text-center' id='register-form' onSubmit={registerUser}> */}
+      <Form className='text-center'>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control
+          type="text" placeholder="Name"
+          maxLength={10} id='name' required={true}/>
+          <br/>
+          <Button type='submit' variant="primary" id="button-addon2">Button</Button>
+        </Form.Group>
+      </Form>
+        {/* <InputGroup className="mb-3">
           <Form.Control
             maxLength={10}
             id='name'
             placeholder="Name"
             aria-label="Name"
             aria-describedby="basic-addon2"
-          />
-          <Button type='submit' variant="outline-secondary" id="button-addon2">
-            Button
-          </Button>
-        </InputGroup>
-      </form>
+          /> */}
+        {/* </InputGroup> */}
+
+      {/* </form> */}
     </Container>
   )
 }
